75f5ff93fffc5d451b1e25b96d8d348f492eec9d
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-03-14 13:23:27 +0100

    Hoisting, Scoping and THIS keyword

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
-
+/*
 // Function declaration
 calculateAge(1965);
 
@@ -31,51 +31,7 @@ function foo() {
 
 foo();
 console.log(age);
-
-
-
-// calculateAge(1990);
-
-// function calculateAge(year) {
-//     console.log(2018 - year);
-// }
-
-
-
-// // This isnt function declaration but expression and HOISTING with functions
-// // Only work for function declaration
-// // So we must comment it out this
-// // retirement(1990);
-
-// var retirement = function(year) {
-//     console.log(65 - (2018 - year));
-// }
-
-
-// // variables
-// // console.log(age);
-
-// console.log(age);
-// var age = 23;
-
-// function foo() {
-//     console.log(age);    
-//     var age = 65;
-//     console.log(age);
-// }
-
-// foo();
-// console.log(age);
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 
@@ -85,32 +41,24 @@ console.log(age);
 
 // First scoping example
 
+/*
+var a = 'Hello!';
+first();
 
-// var a = 'Hello!';
-// first();
+function first() {
+    var b = 'Hi!';
+    second();
 
-// function first() {
-//     var b = 'Hi!';
-//     second();
-
-//     function second() {
-//         var c = 'Hey!';
-//         third();
-//     }
-// }
-
-// function third() {
-//     var d = 'John';
-//     // console.log(c);
-//     console.log(a+d); // We have access to a and d because they're in the scope chain for the 3rd function
-    
-// }
-
+    function second() {
+        var c = 'Hey!';
+        console.log(a, b, c);
+    }
+}
+*/
 
 
 
 // Example to show the differece between execution stack and scope chain
-
 /*
 var a = 'Hello!';
 first();
@@ -127,9 +75,12 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    // console.log(c);    
+    console.log(a+d);
 }
 */
+
+
 
 
 
@@ -141,45 +92,37 @@ function third() {
 /*
 calculateAge(1985);
 
-// regular function not a method. this keyword always points to the window object
-// Because the object of the function that this object attached to
-// Is the global object
 function calculateAge(year) {
     console.log(2016 - year);
     console.log(this);
 }
-
-
+*/
 
 var john = {
-    name: 'John',
-    yearOfBirth: 1990,
-    calculateAge: function() {
+   name: 'John',
+   yearOfBirth: 1990,
+   calculateAge: function() {
         console.log(this);
         console.log(2016 - this.yearOfBirth);
 
-        
+        /*
         function innerFunction() {
-            console.log(this);
+            console.log(this)
         }
+
         innerFunction();
-        
-    }
-}
+        */
+   } 
+};
 
 john.calculateAge();
-
 
 var mike = {
     name: 'Mike',
     yearOfBirth: 1984
 };
 
-// Method borrowing
 mike.calculateAge = john.calculateAge;
 mike.calculateAge();
-
-
-*/
 
 
